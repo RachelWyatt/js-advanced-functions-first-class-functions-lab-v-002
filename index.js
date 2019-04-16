62c93c1a65d6ba03f1707ac873c2e1b568ffb6a8
Rachel Wyatt <rw126@evansville.edu>
2019-04-16 17:43:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const firstFunction = function returnFirstTwoDrivers(drivers) {
+function returnFirstTwoDrivers(drivers) {
   let first_two = []
   first_two.push(drivers[0])
   first_two.push(drivers[1])
