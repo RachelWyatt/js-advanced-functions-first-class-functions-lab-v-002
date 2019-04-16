fba72713953df52451ca1ef99e55ab7e25369b6c
Rachel Wyatt <rw126@evansville.edu>
2019-04-16 17:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
   let first_two = []
-  first_two.push(drivers[0] && drivers[1])
-  first_two
+  first_two.push(drivers[0])
+  first_two.push(drivers[1])
   return first_two
 }
